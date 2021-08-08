7bdfd8df1745be601d273ea21770c5352a4cf5d9
minwoo <qfaq159@naver.com>
2021-08-08 15:00:11 +0900

    FEAT : loginForm 중복 요청 방지 기능 추가 (임시)

--- src/components/loginForm/loginForm.jsx
+++ src/components/loginForm/loginForm.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "./loginForm.module.css";
 
 const LoginForm = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [disabled, setDisabled] = useState(false);
+    const handleSubmit = async(e) =>{
+        setDisabled(true);
+        e.preventDefault();
+        await new Promise((r) => setTimeout(r, 1000));
+        setDisabled(false);
+    }
+    
     return(
         <section className={styles.loginContent}>
-            <form className={styles.loginForm} >
+            <form  className={styles.loginForm} onSubmit={handleSubmit}>
                 <h2>로그인</h2>
-                <input type="email" id="email" name="email" placeholder="email typing..." autoFocus required />
-                <input type="password" id="password" name="password" placeholder="password typing..." autoFocus required />
-                <input type="submit" value="로그인"/>
+                <input type="email" id="email" name="email" placeholder="email typing..." value={email} autoFocus required onChange={e=>setEmail(e.target.value)}/>
+                <input type="password" id="password" name="password" placeholder="password typing..."  value={password} autoFocus required onChange={e=>setPassword(e.target.value)}/>
+                <input type="submit" value="로그인" disabled={disabled}/>
             </form>
         </section>
     )
