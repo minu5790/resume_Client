ff4bc1acd9147e4165993185e7210e150efb9a07
minwoo <qfaq159@naver.com>
2021-08-13 17:50:35 +0900

    FEAT : 리덕스 초기설정

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+
 import App from './app';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>  
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ src/modules/auth.js
@@ -0,0 +1,16 @@
+import { createAction, handleActions } from 'redux-actions';
+
+const SAMPLE_ACTION = 'auth/SAMPLE_ACTION';
+
+export const sampleAction = createAction(SAMPLE_ACTION);
+
+const initState = {};
+
+const auth = handleActions(
+    {
+        [SAMPLE_ACTION]: (state, action) => state,
+    },
+    initState
+)
+
+export default auth;
